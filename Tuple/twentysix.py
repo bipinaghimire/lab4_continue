e41ab0ba9a981dbd9fedd90e8828ae739edb49f4
user <bipinaghimire6@gmail.com>
2021-07-04 22:16:36 +0545

    selective elements

---
+++ Tuple/twentysix.py
@@ -0,0 +1,9 @@
+'''
+Write a Python program to get the 4th element and 4th element from last of a tuple.
+'''
+tup = 1,2,3,4,5,6
+print(tup)
+a= tup[3]
+print(f'fourth element is {a}')
+b= tup[-4]
+print(f'fourth element from last is {b}')
