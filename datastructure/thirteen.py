f4674c045beb763755bddde0cd48aeedb8a2490f
user <bipinaghimire6@gmail.com>
2021-07-04 21:20:28 +0545

    largest from list

--- datastructure/thirteen.py
+++ datastructure/thirteen.py
@@ -1,5 +1,5 @@
 '''
-Write a Python program to get the largest number from a list.
+Write a python program to get the largest number from a list.
 '''
 l=[10, 20, 30, 40, 50, 70, 90, 60, 80]
 l.sort()
