f3305f025f901d034f4797ed46095c5d9f046042
user <bipinaghimire6@gmail.com>
2021-07-05 08:19:14 +0545

    add member in tuple

--- Tuple/twentyfour.py
+++ Tuple/twentyfour.py
@@ -2,5 +2,7 @@
 Write a Python program to add an item in a tuple.
 '''
 tup= (1,2,3,4,5)
+print(tup)
 tup = tup + (6,)
 print(tup)
+print(type(tup))
---
+++ set/twentynine.py
@@ -0,0 +1,3 @@
+'''
+Write a Python program to iteration over sets
+'''
