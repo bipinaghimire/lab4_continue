c07d729c8e3ffaf73ee98a629d7a5ac3a2efe634
user <bipinaghimire6@gmail.com>
2021-07-10 17:29:11 +0545

    iterate

---
+++ dictionary/fourtytwo.py
@@ -0,0 +1,10 @@
+'''
+.Write a Python program to iterate over dictionaries using for loops.
+'''
+dict={1:'cow',2:'tiger',3:'lion',4:'monkey'}
+for i in dict:
+    print(i)
+for j in dict.values():
+        print(j)
+for k in dict.items():
+    print(k)
