13e619d6e24ee8ffe1cb8d42f5fe4e13ea1a18c0
user <bipinaghimire6@gmail.com>
2021-07-10 17:13:06 +0545

    iterate in dictionary

---
+++ dictionary/thirtyeight.py
@@ -0,0 +1,10 @@
+'''
+Write a Python program to iterate over dictionaries using for loops.
+'''
+dict={1:'apple',2:'banana',3:'coconut'}
+for i in dict:
+    print(i)
+for j in dict.values():
+        print(j)
+for k in dict.items():
+    print(k)
