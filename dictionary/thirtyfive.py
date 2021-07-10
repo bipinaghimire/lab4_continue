9c5ba083f2d37d3ed6879b3e5f9ce73b25fb9b10
user <bipinaghimire6@gmail.com>
2021-07-10 16:37:33 +0545

    adding member in dictionary

---
+++ dictionary/thirtyfive.py
@@ -0,0 +1,9 @@
+'''
+Write a Python script to add a key to a dictionary.
+Sample Dictionary : {0: 10, 1: 20}
+Expected Result :{0: 10, 1: 20, 2: 30}
+'''
+d={1:'apple', 2:'banana', 3:'coconut'}
+print(d)
+d[4]='lemon'
+print('dictionary with added member is',d)
