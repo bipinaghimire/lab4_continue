ac30f37eb5b3a75bed6420450aa85d8fbedc883b
user <bipinaghimire6@gmail.com>
2021-07-10 17:26:56 +0545

    merging two dictionaries

--- dictionary/fourtyone.py
+++ dictionary/fourtyone.py
@@ -3,4 +3,6 @@ Write a Python script to merge two Python dictionaries.
 '''
 d1={1:'apple', 2:'banana', 3:'mango'}
 d2={4:'coconut',5:'litchi',6:'grapes'}
-print(d1|d2)
+result= d1|d2
+print(result)
+print(type(result))
